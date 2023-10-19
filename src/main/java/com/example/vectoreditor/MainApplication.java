e88ed5480fcf21941e4b74ee53eeab273a1f623a
cyerim0314 <cyerim0314@naver.com>
2023-10-19 15:50:58 +0900

    Update MainApplication.java
    화면 크기 조정

--- src/main/java/com/example/vectoreditor/MainApplication.java
+++ src/main/java/com/example/vectoreditor/MainApplication.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
 
 public class MainApplication extends Application {
     public static void main(String[] args) {
@@ -17,10 +19,18 @@ public class MainApplication extends Application {
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("app.fxml"));
         Parent root = loader.load();
-        Scene scene = new Scene(root, 800, 600);
+        
+        double screenWidth = (Screen.getPrimary().getBounds().getWidth())*0.9;
+        double screenHeight = (Screen.getPrimary().getBounds().getHeight())*0.9;
+
+        // 1500과 screenWidth 중 큰 값을 선택
+        double finalWidth = Math.max(1500, screenWidth);
+        // 800과 screenHeight 중 큰 값을 선택
+        double finalHeight = Math.max(800, screenHeight);
+
+        Scene scene = new Scene(root, finalWidth, finalHeight);
         primaryStage.setScene(scene);
 
         primaryStage.show();
     }
-
 }
